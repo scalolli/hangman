f2b8b3f2791bd1575189154b620fa4a9be204d75
scalolli <basavarajkalloli@gmail.com>
2018-04-13 11:39:21 +0100

    getting hangman to work

--- src/Main.hs
+++ src/Main.hs
@@ -9,10 +9,6 @@ module Main where
 
   type WordList = [String]
 
-  main :: IO ()
-  main = do
-    putStrLn "hello world"
-
   allWords :: IO WordList
   allWords = do
     dict <- readFile "data/words"
@@ -95,4 +91,42 @@ module Main where
           putStrLn "This character wasn't in\
                    \ the word, try again."
           return (fillInCharacter puzzle guess)
+
+
+  gameOver :: Puzzle -> IO ()
+  gameOver (Puzzle wordToGuess _ guessed) =
+      if (length guessed) > 7 then
+        do putStrLn "You lose!"
+           putStrLn $ "The word was: " ++ wordToGuess
+           exitSuccess
+      else
+        return ()
+
+  gameWin :: Puzzle -> IO ()
+  gameWin (Puzzle _ guessedSoFar _) =
+    if all isJust guessedSoFar then
+      do
+        putStrLn "You Win!"
+        exitSuccess
+    else
+      return ()
+
+  runGame :: Puzzle -> IO ()
+  runGame puzzle = forever $ do
+      gameOver puzzle
+      gameWin puzzle
+      putStrLn $
+        "Current puzzle is: " ++ show puzzle
+      putStr "Guess a letter: "
+      guess <- getLine
+      case guess of
+        [c] -> handleGuess puzzle c >>= runGame
+        _ ->
+            putStrLn "Your guess must be a single character"
+
+  main :: IO ()
+  main = do
+    word <- randomWord'
+    let puzzle = freshPuzzle (fmap toLower word)
+    runGame puzzle
 
